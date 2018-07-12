950788fa9fcc73be132a4e1d73b46f93f3e0c667
原朋子 <aodreyhappy@gmail.com>
2018-07-12 17:36:53 +0900

    カードを画像にした

--- js/card.js
+++ js/card.js
@@ -11,10 +11,19 @@ Card.prototype.getDisplayName = function() {
     if (this.isFaceUp) {
 	return this.mark + this.number;
     } else {
-	return "＊＊＊";
+	return "downturned";
     }
   
 };
+Card.prototype.getDisplayImage = function() {
+    if (this.isFaceUp) {
+        // ゼロパディング
+        var n = ( '00' + this.number ).slice( -2 );
+        return "img/cards/" + this.mark + n + ".png";
+    } else {
+        return "img/cards/downturned.png";
+    }
+}
 Card.prototype.turn = function() {
     this.isFaceUp = !this.isFaceUp;
 };
--- js/index.js
+++ js/index.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
     set.shuffle();
     // 全部のカード
     for (var i = 0; i < set.array.length; i++){
-	// 表示
-	$("#card"+i).html(set.array[i].getDisplayName());
+	   // 表示
+	$("#card"+i + " img").attr("src",set.array[i].getDisplayImage());
     }
-          
-    // クリックでひっくりかえす
+    
+    // クリック
     $(".turn").click(function(){
-    	var id =  $(this).attr("id");
-	var i = id.slice(4); // "card52" から "52" を取り出す
+        var id =  $(this).attr("id");
+        var i = id.slice(4); // "card52" から "52" を取り出す
+        
+        // ひっくり返す
     	set.array[i].isFaceUp = ! set.array[i].isFaceUp;
-        $("#"+id).html(set.array[i].getDisplayName());
+        
+        // 再表示
+        $("#"+id + " img").attr("src",set.array[i].getDisplayImage());
     });
 });
